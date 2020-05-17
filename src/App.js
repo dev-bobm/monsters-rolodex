210d1f452fc68fed63e96990e088f7b4a95139d5
Bob Molenbroek <lourens@xs4all.nl>
2020-05-17 13:13:28 +0200

    Added error catch to fetch setting

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ class App extends Component {
   componentDidMount() {
     fetch('https:/jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
+      .then((users) => this.setState({ monsters: users }))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
   handleChange = (e) => {
     this.setState({ searchField: e.target.value });
